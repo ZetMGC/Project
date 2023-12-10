1e8183837e4f2b917187dd707362e91a421c9062
ZetMGC <44321134+ZetMGC@users.noreply.github.com>
2023-12-10 21:30:22 +0300

    testing vue

---
+++ project/src/components/PostList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h3>Список пользователей</h3>
+        <post-item v-for="post in posts" :post="post" :key="post"/>
+    </div>
+</template>
+
+<script>
+import PostItem from './PostItem.vue';
+export default {
+    components: {PostItem},
+    props: {
+        posts: {
+            type: Array,
+            required: true
+        }
+    },
+}
+</script>
+
+<style scoped>
+.post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+}
+</style>
