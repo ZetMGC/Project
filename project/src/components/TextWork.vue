743d7e17d5fdef830ccf33b1d4b0a156a35b0a8c
dionysiusmukha <dionysiusmukha@gmail.com>
2023-12-14 13:17:01 +0300

    4124

---
+++ project/src/components/TextWork.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+  <div id="weWork">
+    <h2> С нами работают </h2>
+  </div>
+  <div id="description" class = "row justify-content-center">
+    <div class ="col-6">
+    <p><small class = "text-body-secondary"> Десятки компаний доверяют нам самое ценное, что у них есть в интернете – свои сайты.
+      Мы делаем всё, чтобы наше сотрудничество было долгим.</small></p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#weWork {
+  font-size: 42px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  text-align: center;
+}
+#description {
+  font-family: Montserrat;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 22px;
+  text-align: center;
+}
+
+
+</style>
